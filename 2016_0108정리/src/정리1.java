9a044bc54432ea8fef0f305c870929f5552b65c8
berger19 <berger27@naver.com>
2016-01-08 16:48:52 +0900

    2016_0108정리

---
+++ 2016_0108정리/src/정리1.java
@@ -0,0 +1,18 @@
+/*
+ * 		1. Data Type
+ * 		2. 연산자(단항, 이항, 삼항)
+ * 		3. 제어문(조건문, 반복문, 반복제어문, switch~case)
+ * 		4. 명령문(관련있는 명령끼리 묶어줌으로써 메소드로 바꿔주는 것)
+ * 			1) 데이터 묶는것(배열)
+ * 			2) 명령어(메소드) // 두가지를 연결하는 것이 클래스
+ * 
+ * 		
+ */
+public class 정리1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
